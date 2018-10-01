729e2af1ae474a93b203d99e1dc9cdee184149fa
minoring <minho1350@gmail.com>
2018-10-01 22:07:23 +0900

    Updated login in json file.

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
+  console.log(req.user);
   let title = "Welcome";
   let body = "Hello!"
   let html = template.HTML(title, body, auth.StatusUI(req, res));
