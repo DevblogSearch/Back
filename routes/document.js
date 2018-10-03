dae91635c244163579ca741a810e3b10f5d6283d
SonGeon <fjen6b@gmail.com>
2018-10-04 01:05:08 +0900

    document post added

---
+++ routes/document.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const solrClient = require('../lib/solr')();
+
+const router = express.Router();
+const documentPostCheck = [ 'url', 'content', 'title' ];
+/*document result*/
+router.post('/', (req, res, next) => {
+  console.log(req.body);
+  for (var elem of req.body){
+    var omittedFlag = false;
+    for (var key of documentPostCheck) {
+      if(!elem.hasOwnProperty(key)) {
+        //must send keys omitted
+        omittedFlag = true;
+        break;
+      }
+    }
+    
+    if(!omittedFlag) {
+      //TODO change solr-client because it doesn't support update using array
+      solrClient.update({ url:elem.url, content:elem.content, title:elem.title })
+        .then(function(result) {
+          return result;
+        }).catch(function(err) {
+          if (err) 
+            console.log(err);
+        });
+    }
+  }
+  res.send('OK');
+  return;
+});
+
+
+module.exports = router;
