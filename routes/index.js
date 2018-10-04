a178d7f476bdcab348b88cd9a36eef5ec308b40c
Jo Ik Hyeon <8982679@kookmin.ac.kr>
2018-10-04 17:38:47 +0900

    no message

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', (req, res, next) => {
   let title = "나랏말싸미";
-  let body = "Hello!"
+  let body = ""
   let html = template.HTML(title, body, auth.StatusUI(req, res));
   res.send(html);
 });
