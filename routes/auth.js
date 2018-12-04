8a2537d0f7ade3bc33dd8c59e17fcb71d1d9f7b4
Jo Ik Hyeon <8982679@kookmin.ac.kr>
2018-12-04 14:13:16 +0900

    로그인 배경화면 추가, 로그인 내용 수정

--- routes/auth.js
+++ routes/auth.js
@@ -8,9 +8,18 @@ module.exports = function (passport) {
 
   router.get('/login', (req, res) => {
     let title = 'WEB - login';
-    let html = template.HTML(title, `
+    let html = template.HTML(title,`
+      <script src="javascript/plugins/geometryangle/geometryangle.min.js"></script>    
       <link rel="stylesheet" href="/stylesheet/login.min.css" type="text/css"/>
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+      <script type="text/javascript">
+      $(document).ready(function(){
+        $('body').Geometryangle({mesh:{}, lights: [{}], line: {}, vertex: {}});
+      });
+      </script>
+    `, `
+      <div style="position: fixed; left: 0;right: 0;top: 0; bottom: 0;"></div>
       <div class="login-form">
       <form action="/auth/login_process" method="post">
       <h2 class="text-center">나랏말싸미</h2><br>		
@@ -18,30 +27,6 @@ module.exports = function (passport) {
         <a href="/auth/facebook" class="btn btn-primary btn-block"><i class="fa fa-facebook"></i> <b>Facebook</b> 으로 로그인</a>
         <a href="/auth/google" class="btn btn-danger btn-block"><i class="fa fa-google"></i> <b>Google</b> 으로 로그인</a>
         <a href="/auth/github" class="btn btn-dark btn-block"><i class="fa fa-github"></i> <b>Github</b> 으로 로그인</a>
-      </div>
-      <div class="or-seperator"></div>
-      <div class="form-group">
-        <div class="input-group">
-          <span class="input-group-addon"><i class="fa fa-user"></i></span>
-          <input type="text" class="form-control" name="email" placeholder="Email" required="required">
-        </div>
-      </div>
-      <div class="form-group">
-        <div class="input-group">
-          <span class="input-group-addon"><i class="fa fa-lock"></i></span>
-          <input type="password" class="form-control" name="pwd" placeholder="Password" required="required">
-        </div>
-      </div>        
-      <div class="form-group"> 
-        <button type="submit" class="btn btn-success btn-block login-btn" value="login">로그인</button>
-      </div>
-      <div class="clearfix">
-        <label class="pull-left checkbox-inline"><input type="checkbox"> 계정 기억하기</label>
-        <a href="#" class="pull-right text-success">비밀번호를 잊으셨나요?</a>
-      </div>  
-      
-      </form>
-        <div class="hint-text small">계정이 없나요? <a href="/auth/register" class="text-success">회원가입</a></div>
       </div>
     `, '', '');
     res.send(html);
